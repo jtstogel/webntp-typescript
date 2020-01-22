d5593fce0b4a210fc05b1a0fffe496060f83d844
Jackson Stogel <jtstogel@berkeley.edu>
2020-01-22 13:02:50 -0800

    more flexible example

--- server-ntp/client.ts
+++ server-ntp/client.ts
@@ -3,7 +3,19 @@ import NTPClient from "../ntp/client";
 import WebSocket from "isomorphic-ws";
 import {SdcMessageEvent} from "../simple-data-channel/simple-data-channel";
 
-const ntpClient = new NTPClient([
-    'ntp-0.webntp.org:6789',
-    'ntp-1.webntp.org:6789',
-]);
+
+
+window.onloadend = () => {
+    // @ts-ignore
+    document.getElementById("button").onclick = () => {
+        // @ts-ignore
+        const hosts = document.getElementById("hosts").innerHTML.trim().split('\n').map(s => s.trim());
+        const ntpClient = new NTPClient(hosts);
+        const output = document.getElementById("output");
+        setInterval(() => {
+            // @ts-ignore
+            output.innerHTML = `offset: ${ntpClient.offset}, jitter: ${ntpClient.jitter}`;
+        }, 2500);
+    }
+};
+
